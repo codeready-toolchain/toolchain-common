6557d9235a97e032945a1b1fb2e382dadc00622f
Alexey Kazakov <alkazako@redhat.com>
2019-08-21 16:02:34 -0700

    Add Get and List to fake client

--- pkg/test/client.go
+++ pkg/test/client.go
@@ -10,15 +10,17 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
-// NewFakeClient creates a fake K8s client with ability to override specific Create/Update/Delete functions
+// NewFakeClient creates a fake K8s client with ability to override specific Get/List/Create/Update/StatusUpdate/Delete functions
 func NewFakeClient(t *testing.T, initObjs ...runtime.Object) *FakeClient {
 	client := fake.NewFakeClientWithScheme(scheme.Scheme, initObjs...)
-	return &FakeClient{client, t, nil, nil, nil, nil}
+	return &FakeClient{client, t, nil, nil, nil, nil, nil, nil}
 }
 
 type FakeClient struct {
 	client.Client
 	T                *testing.T
+	MockGet          func(key client.ObjectKey, obj runtime.Object) error
+	MockList         func(opts *client.ListOptions, list runtime.Object) error
 	MockCreate       func(obj runtime.Object) error
 	MockUpdate       func(obj runtime.Object) error
 	MockStatusUpdate func(obj runtime.Object) error
@@ -31,6 +33,20 @@ type mockStatusUpdate struct {
 
 func (m *mockStatusUpdate) Update(ctx context.Context, obj runtime.Object) error {
 	return m.mockUpdate(obj)
+}
+
+func (c *FakeClient) Get(ctx context.Context, key client.ObjectKey, obj runtime.Object) error {
+	if c.MockGet != nil {
+		return c.MockGet(key, obj)
+	}
+	return c.Client.Get(ctx, key, obj)
+}
+
+func (c *FakeClient) List(ctx context.Context, opts *client.ListOptions, list runtime.Object) error {
+	if c.MockList != nil {
+		return c.MockList(opts, list)
+	}
+	return c.Client.List(ctx, opts, list)
 }
 
 func (c *FakeClient) Create(ctx context.Context, obj runtime.Object) error {
