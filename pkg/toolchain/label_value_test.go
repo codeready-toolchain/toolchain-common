c53864a466a584bebc06050b27b064c0d94df582
tinakurian <tkurian@redhat.com>
2019-12-05 15:51:33 -0500

    removing subtest

--- pkg/toolchain/label_value_test.go
+++ pkg/toolchain/label_value_test.go
@@ -9,17 +9,15 @@ import (
 )
 
 func TestLabelValue(t *testing.T) {
-	t.Run("string to valid value", func(t *testing.T) {
-		value := toolchain.ToValidValue("johndoe")
-		assert.Equal(t, value, "johndoe")
+	value := toolchain.ToValidValue("johndoe")
+	assert.Equal(t, value, "johndoe")
 
-		value = toolchain.ToValidValue("johndoe-at-test-dot-com")
-		assert.Equal(t, value, "johndoe-at-test-dot-com")
+	value = toolchain.ToValidValue("johndoe-at-test-dot-com")
+	assert.Equal(t, value, "johndoe-at-test-dot-com")
 
-		value = toolchain.ToValidValue("johndoe@test.com")
-		assert.Equal(t, value, "johndoe-at-test-com")
+	value = toolchain.ToValidValue("johndoe@test.com")
+	assert.Equal(t, value, "johndoe-at-test-com")
 
-		value = toolchain.ToValidValue("john.jane.doe@test.com")
-		assert.Equal(t, value, "john-jane-doe-at-test-com")
-	})
+	value = toolchain.ToValidValue("john.jane.doe@test.com")
+	assert.Equal(t, value, "john-jane-doe-at-test-com")
 }
