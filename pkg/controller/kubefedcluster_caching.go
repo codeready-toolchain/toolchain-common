6115eeedb92a8e7d5b76468040ad45cb2c352368
Matous Jobanek <mjobanek@redhat.com>
2020-01-03 22:16:51 +0000

    fix(CRT-397): use standard informer for KubeFedCluster_cache controller

--- pkg/controller/kubefedcluster_caching.go
+++ pkg/controller/kubefedcluster_caching.go
@@ -2,7 +2,10 @@ package controller
 
 import (
 	"github.com/codeready-toolchain/toolchain-common/pkg/cluster"
+	"k8s.io/apimachinery/pkg/fields"
+	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/cache"
+	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/kubefed/pkg/apis/core/v1beta1"
@@ -11,22 +14,28 @@ import (
 
 func StartCachingController(mgr manager.Manager, namespace string, stopChan <-chan struct{}) error {
 	cntrlName := "controller_kubefedcluster_with_cache"
-	clusterCacheService := cluster.NewKubeFedClusterService(mgr.GetClient(), logf.Log.WithName(cntrlName), namespace)
 
-	_, clusterController, err := util.NewGenericInformerWithEventHandler(
-		mgr.GetConfig(),
-		namespace,
-		&v1beta1.KubeFedCluster{},
-		util.NoResyncPeriod,
-		&cache.ResourceEventHandlerFuncs{
-			DeleteFunc: clusterCacheService.DeleteKubeFedCluster,
-			AddFunc:    clusterCacheService.AddKubeFedCluster,
-			UpdateFunc: clusterCacheService.UpdateKubeFedCluster,
-		},
-	)
+	clusterCacheService := cluster.NewKubeFedClusterService(mgr.GetClient(), logf.Log.WithName(cntrlName), namespace)
+	eventHandler := &cache.ResourceEventHandlerFuncs{
+		DeleteFunc: clusterCacheService.DeleteKubeFedCluster,
+		AddFunc:    clusterCacheService.AddKubeFedCluster,
+		UpdateFunc: clusterCacheService.UpdateKubeFedCluster,
+	}
+
+	gvk, err := apiutil.GVKForObject(&v1beta1.KubeFedCluster{}, mgr.GetScheme())
 	if err != nil {
 		return err
 	}
+
+	client, err := apiutil.RESTClientForGVK(gvk, mgr.GetConfig(), scheme.Codecs)
+	if err != nil {
+		return err
+	}
+
+	listWatch := cache.NewListWatchFromClient(client, "kubefedclusters", namespace, fields.Everything())
+
+	_, clusterController := cache.NewInformer(listWatch, &v1beta1.KubeFedCluster{}, util.NoResyncPeriod, eventHandler)
+
 	logf.Log.Info("Starting Controller", "controller", cntrlName)
 	go clusterController.Run(stopChan)
 	return nil
