7cfcb9dfbe1612a10a196b10af8eaed012340aad
tinakurian <tkurian@redhat.com>
2020-02-13 12:19:19 -0500

    fixing hasEmbeddedSpec and HasSpec

--- pkg/test/useraccount/assertion.go
+++ pkg/test/useraccount/assertion.go
@@ -9,7 +9,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"reflect"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"testing"
 )
@@ -55,14 +54,14 @@ func (a *Assertion) Exists() *Assertion {
 func (a *Assertion) HasEmbeddedSpec(spec toolchainv1alpha1.UserAccountSpecEmbedded) *Assertion {
 	err := a.loadUaAssertion()
 	require.NoError(a.t, err)
-	reflect.DeepEqual(spec.UserAccountSpecBase, a.userAccount.Spec)
+	assert.Equal(a.t, spec.UserAccountSpecBase, a.userAccount.Spec)
 	return a
 }
 
 func (a *Assertion) HasSpec(spec toolchainv1alpha1.UserAccountSpec) *Assertion {
 	err := a.loadUaAssertion()
 	require.NoError(a.t, err)
-	reflect.DeepEqual(spec, a.userAccount.Spec)
+	assert.Equal(a.t, spec, a.userAccount.Spec)
 	return a
 }
 
