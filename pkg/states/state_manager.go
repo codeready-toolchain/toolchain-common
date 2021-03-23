638dcea1b84d86eb4d1e8967e97d20335d407974
Shane Bryzak <sbryzak@gmail.com>
2021-03-23 19:21:00 +1000

    CRT-994 refactored, updated tests
    Signed-off-by: Shane Bryzak <sbryzak@gmail.com>

--- pkg/states/state_manager.go
+++ pkg/states/state_manager.go
@@ -2,18 +2,66 @@ package states
 
 import "github.com/codeready-toolchain/api/pkg/apis/toolchain/v1alpha1"
 
+func Active(userSignup *v1alpha1.UserSignup) bool {
+	return Approved(userSignup) &&
+		!VerificationRequired(userSignup) &&
+		!Deactivated(userSignup)
+}
+
+func Approved(userSignup *v1alpha1.UserSignup) bool {
+	return contains(userSignup.Spec.States, v1alpha1.UserSignupStateApproved)
+}
+
+func SetApproved(userSignup *v1alpha1.UserSignup, val bool) {
+	if val && !contains(userSignup.Spec.States, v1alpha1.UserSignupStateApproved) {
+		userSignup.Spec.States = append(userSignup.Spec.States, v1alpha1.UserSignupStateApproved)
+	}
+
+	if !val && contains(userSignup.Spec.States, v1alpha1.UserSignupStateApproved) {
+		userSignup.Spec.States = remove(userSignup.Spec.States, v1alpha1.UserSignupStateApproved)
+	}
+}
+
+func VerificationRequired(userSignup *v1alpha1.UserSignup) bool {
+	return contains(userSignup.Spec.States, v1alpha1.UserSignupStateVerificationRequired)
+}
+
+func SetVerificationRequired(userSignup *v1alpha1.UserSignup, val bool) {
+	if val && !contains(userSignup.Spec.States, v1alpha1.UserSignupStateVerificationRequired) {
+		userSignup.Spec.States = append(userSignup.Spec.States, v1alpha1.UserSignupStateVerificationRequired)
+	}
+
+	if !val && contains(userSignup.Spec.States, v1alpha1.UserSignupStateVerificationRequired) {
+		userSignup.Spec.States = remove(userSignup.Spec.States, v1alpha1.UserSignupStateVerificationRequired)
+	}
+}
+
 func Deactivating(userSignup *v1alpha1.UserSignup) bool {
 	return contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivating)
 }
 
-func SetUserSignupState(userSignup *v1alpha1.UserSignup, state v1alpha1.UserSignupState) {
-	if !contains(userSignup.Spec.States, state) {
-		userSignup.Spec.States = append(userSignup.Spec.States, state)
+func SetDeactivating(userSignup *v1alpha1.UserSignup, val bool) {
+	if val && !contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivating) {
+		userSignup.Spec.States = append(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivating)
+	}
+
+	if !val && contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivating) {
+		userSignup.Spec.States = remove(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivating)
 	}
 }
 
-func UnsetUserSignupState(userSignup *v1alpha1.UserSignup, state v1alpha1.UserSignupState) {
-	userSignup.Spec.States = remove(userSignup.Spec.States, state)
+func Deactivated(userSignup *v1alpha1.UserSignup) bool {
+	return contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivated)
+}
+
+func SetDeactivated(userSignup *v1alpha1.UserSignup, val bool) {
+	if val && !contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivated) {
+		userSignup.Spec.States = append(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivated)
+	}
+
+	if !val && contains(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivated) {
+		userSignup.Spec.States = remove(userSignup.Spec.States, v1alpha1.UserSignupStateDeactivated)
+	}
 }
 
 func contains(s []v1alpha1.UserSignupState, state v1alpha1.UserSignupState) bool {
--- pkg/states/state_manager_test.go
+++ pkg/states/state_manager_test.go
@@ -10,32 +10,93 @@ func TestStateManager(t *testing.T) {
 
 	u := &v1alpha1.UserSignup{}
 
-	t.Run("test set UserSignup state", func(t *testing.T) {
+	t.Run("test approved", func(t *testing.T) {
 
-		SetUserSignupState(u, v1alpha1.UserSignupStateDeactivating)
+		SetApproved(u, true)
 
+		require.True(t, Approved(u))
 		require.Len(t, u.Spec.States, 1)
-		require.Equal(t, v1alpha1.UserSignupStateDeactivating, u.Spec.States[0])
-	})
+		require.Equal(t, v1alpha1.UserSignupStateApproved, u.Spec.States[0])
 
-	t.Run("test unset UserSignup state", func(t *testing.T) {
-
-		// Try to unset a state that isn't set
-		UnsetUserSignupState(u, v1alpha1.UserSignupStateBanned)
-
-		// Ensure the existing states haven't changed
-		require.Len(t, u.Spec.States, 1)
-
-		UnsetUserSignupState(u, v1alpha1.UserSignupStateDeactivating)
+		SetApproved(u, false)
 
 		require.Len(t, u.Spec.States, 0)
+		require.False(t, Approved(u))
 	})
 
-	t.Run("test Deactivating function", func(t *testing.T) {
-		require.False(t, Deactivating(u))
+	t.Run("test verification required", func(t *testing.T) {
 
-		SetUserSignupState(u, v1alpha1.UserSignupStateDeactivating)
+		SetVerificationRequired(u, true)
+
+		require.True(t, VerificationRequired(u))
+
+		require.False(t, Approved(u))
+		require.Len(t, u.Spec.States, 1)
+		require.Equal(t, v1alpha1.UserSignupStateVerificationRequired, u.Spec.States[0])
+
+		SetVerificationRequired(u, false)
+
+		require.Len(t, u.Spec.States, 0)
+		require.False(t, VerificationRequired(u))
+	})
+
+	t.Run("test deactivating", func(t *testing.T) {
+		SetDeactivating(u, true)
 
 		require.True(t, Deactivating(u))
+
+		require.False(t, Deactivated(u))
+		require.Len(t, u.Spec.States, 1)
+		require.Equal(t, v1alpha1.UserSignupStateDeactivating, u.Spec.States[0])
+
+		SetDeactivating(u, false)
+
+		require.Len(t, u.Spec.States, 0)
+		require.False(t, Deactivating(u))
+	})
+
+	t.Run("test deactivated", func(t *testing.T) {
+		SetDeactivating(u, true)
+
+		require.True(t, Deactivating(u))
+
+		require.False(t, Deactivated(u))
+		require.Len(t, u.Spec.States, 1)
+		require.Equal(t, v1alpha1.UserSignupStateDeactivating, u.Spec.States[0])
+
+		SetDeactivating(u, false)
+
+		require.Len(t, u.Spec.States, 0)
+		require.False(t, Deactivating(u))
+	})
+
+	t.Run("test active", func(t *testing.T) {
+		u = &v1alpha1.UserSignup{}
+		// Should not be active by default
+		require.False(t, Active(u))
+
+		SetApproved(u, true)
+		// Should be active when approved
+		require.True(t, Active(u))
+
+		SetVerificationRequired(u, true)
+		// Should not be active when verification is required
+		require.False(t, Active(u))
+
+		SetDeactivated(u, true)
+		// Should not be active when deactivated
+		require.False(t, Active(u))
+
+		SetVerificationRequired(u, false)
+		// Should still not be active
+		require.False(t, Active(u))
+
+		SetDeactivated(u, false)
+		// Should be active when verification not required and not deactivated
+		require.True(t, Active(u))
+
+		SetDeactivating(u, true)
+		// Should be active when deactivating
+		require.True(t, Active(u))
 	})
 }
