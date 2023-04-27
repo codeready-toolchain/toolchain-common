801d7f82b8e5e529053014c6c533d78ba63932e7
Kanika Rana <krana@redhat.com>
2023-04-27 00:18:59 -0400

    update comments and tests, maxlength exportable

--- pkg/usersignup/usersignup.go
+++ pkg/usersignup/usersignup.go
@@ -9,9 +9,10 @@ var (
 	specialCharRegexp = regexp.MustCompile("[^A-Za-z0-9]")
 	onlyNumbers       = regexp.MustCompile("^[0-9]*$")
 	dnsRegexp         = regexp.MustCompile("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$")
-	// Maximum Length for compliant username is limited to 19 characters such that the result namespace of the type <compliantUsername>-<ns_suffix> is less than 30 characters, to be dns compliant.
-	// With the AppStudio tier the longest suffix is -tenant, which is 7 characters, but with subspaces an additional -env is 4 characters.
-	maxLength = 19
+	// Maximum Length for compliant username is limited to 20 characters such that the result namespace of the type "<compliantUsername>-<ns_suffix>" is less than 30 characters, to be dns compliant.
+	// With the AppStudio tier the longest suffix is "-tenant", which is 7 characters, but with subspaces <ns_suffix> is of type "<generatedsuffix(5chars)>-env" making it the longer suffix of 10 chars.
+	// Thus the maximum length of compliantUsername should be 30-10 = 20 chars.
+	MaxLength = 20
 )
 
 func TransformUsername(username string) string {
@@ -34,8 +35,8 @@ func TransformUsername(username string) string {
 	if strings.HasSuffix(newUsername, "-") {
 		newUsername = newUsername + "crt"
 	}
-	if len(newUsername) > maxLength {
-		newUsername = newUsername[0:maxLength]
+	if len(newUsername) > MaxLength {
+		newUsername = newUsername[0:MaxLength]
 		if !dnsRegexp.MatchString(newUsername) {
 			// trim starting or ending hyphen
 			newUsername = strings.Trim(newUsername, "-")
--- pkg/usersignup/usersignup_test.go
+++ pkg/usersignup/usersignup_test.go
@@ -31,13 +31,12 @@ func TestTransformUsername(t *testing.T) {
 	assertName(t, "crt-me-crt", "_me_")
 	assertName(t, "crt-me-crt", "-me-")
 	assertName(t, "crt-12345", "12345")
-	assertName(t, "thisisabout19charac", "thisisabout19characters@email.com")
-	assertName(t, "isexactly19charactr", "isexactly19charactr@email.com")
-	assertName(t, "isexactly19charactr", "isexactly19charactr")
-	assertName(t, "isexactly21characte", "isexactly21characte-r")
-	assertName(t, "isexactly20charactr", "isexactly20charactr-")
-	assertName(t, "thisis19characters", "thisis19characters-")
-	assertName(t, "thisis20characters1", "thisis20characters1-")
+	assertName(t, "thisisabout20charact", "thisisabout20characters@email.com")
+	assertName(t, "isexactly20character", "isexactly20character@email.com")
+	assertName(t, "isexactly20character", "isexactly20character")
+	assertName(t, "isexactly21characte", "isexactly21characte-r") // shortened 20 character will end in hyphen
+	assertName(t, "isexactly20charactr", "isexactly20charactr-")  // but ending in hyphen
+	assertName(t, "thisis19characters-c", "thisis19characters-")  // suffix -crt is added before truncating string
 }
 
 var dnsRegExp = "^[a-z0-9]([-a-z0-9]*[a-z0-9])?$"
