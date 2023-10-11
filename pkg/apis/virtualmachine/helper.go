0afed357b97f31d70aefb5e248fd08b5b66d3d3a
Rajiv Senthilnathan <rajivnathan@gmail.com>
2023-10-10 21:06:42 -0400

    Add newVM func

--- pkg/apis/virtualmachine/helper.go
+++ pkg/apis/virtualmachine/helper.go
@@ -2,7 +2,33 @@ package virtualmachine
 
 import (
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func NewVM(name, namespace string, options ...VMOption) *VirtualMachine {
+	vm := &VirtualMachine{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: VirtualMachineSpec{
+			Template: &VirtualMachineInstanceTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: name,
+				},
+				Spec: VirtualMachineInstanceSpec{
+					Domain: DomainSpec{},
+				},
+			},
+		},
+	}
+
+	for _, option := range options {
+		option(vm)
+	}
+
+	return vm
+}
 
 type VMOption func(*VirtualMachine)
 
