087915c41e9d35be342c639892ec4e9308836b4e
Lukas Krejci <lkrejci@redhat.com>
2024-01-25 17:17:23 +0100

    Create `assertions` package that defines generic predicates and AssertThat function to work on any client.Object.
    This enables the reuse of the predicates both in the helper functions for
    unit tests (that can use the AssertThat function) as well as in end-to-end
    tests (which can use wait.For().FirstThat()).
    
    This enables us to reuse the same predicates across the whole testsuite
    instead of having one set for unit tests and another set for the end-to-end
    tests.

---
+++ pkg/test/assertions/predicates.go
@@ -0,0 +1,134 @@
+package assertions
+
+import (
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Predicate is a generic predicate for testing whether some object of type T has some quality.
+// It is best used with the `AssertThat` function or with the `wait.For(...).FirstThat(...)`
+// function in end to end tests using the Is function as a helper to satisfy the method
+// signatures and help Go's type inference.
+//
+// Note that if you're implementing your own predicates, it is helpful for the constructor
+// function to not return a concrete type but the generic Predicate[YourObjectType]. This
+// helps the Go compiler to be able to infer and match up the types correctly.
+//
+// E.g. if one would want to implement a predicate checking that a ToolchainCluster CR has
+// the ready condition checked, one might implement a constructor function for that predicate
+// like this:
+//
+//	type toochainClusterReady struct {}
+//
+//	func (t *toolchainClusterReady) Matches(c *toolchainv1alpha1.ToolchainCluster) bool {
+//	  return condition.IsTrue(c.Status.Conditions, toolchainv1alpha1.ConditionReady)
+//	}
+//
+//	func Ready() predicates.Predicate[*toolchainv1alpha1.ToolchainCluster] {
+//	  return &toolchainClusterReady{}
+//	}
+//
+// Such predicate can then easily be used with the `AssertThat` function (or
+// `wait.For(...).FirstThat(...)` from toolchain-e2e which does something very similar
+// but waits for an object that satisfies the predicates to appear in the cluster).
+//
+//	assertions.AssertThat(t, toolchainCluster, assertions.Is(Ready()))
+type Predicate[T client.Object] interface {
+	Matches(obj T) bool
+}
+
+// Is merely casts the generic predicate on type T to a predicate on client.Object. This is
+// always valid because T is required to implement client.Object. Using this function helps
+// readability of the code by being able to construct expressions like:
+//
+//	predicates.Is(predicates.Named("whatevs"))
+func Is[T client.Object](p Predicate[T]) Predicate[client.Object] {
+	return isCast[T]{inner: p}
+}
+
+type isCast[T client.Object] struct {
+	inner Predicate[T]
+}
+
+func (i isCast[T]) Matches(obj client.Object) bool {
+	return i.inner.Matches(obj.(T))
+}
+
+type named struct {
+	name string
+}
+
+func (n *named) Matches(obj client.Object) bool {
+	return obj.GetName() == n.name
+}
+
+// Named returns a predicate checking that an Object has given name.
+func Named(name string) Predicate[client.Object] {
+	return &named{name: name}
+}
+
+type inNamespace struct {
+	namespace string
+}
+
+func (i *inNamespace) Matches(obj client.Object) bool {
+	return obj.GetNamespace() == i.namespace
+}
+
+// InNamespace returns a predicate checking that an Object is in the given namespace.
+func InNamespace(name string) Predicate[client.Object] {
+	return &inNamespace{namespace: name}
+}
+
+type withKey struct {
+	types.NamespacedName
+}
+
+func (w *withKey) Matches(obj client.Object) bool {
+	return obj.GetName() == w.Name && obj.GetNamespace() == w.Namespace
+}
+
+// HasObjectKey returns a predicate checking that an Object has given NamespacedName (aka client.ObjectKey).
+func HasObjectKey(key types.NamespacedName) Predicate[client.Object] {
+	return &withKey{NamespacedName: key}
+}
+
+type hasLabels struct {
+	requiredLabels map[string]string
+}
+
+func (h *hasLabels) Matches(obj client.Object) bool {
+	objLabels := obj.GetLabels()
+	for k, v := range h.requiredLabels {
+		value, present := objLabels[k]
+		if !present || value != v {
+			return false
+		}
+	}
+	return true
+}
+
+// HasLabels returns a predicate checking that an Object has provided labels and their values.
+func HasLabels(requiredLabels map[string]string) Predicate[client.Object] {
+	return &hasLabels{requiredLabels: requiredLabels}
+}
+
+type hasAnnotations struct {
+	requiredAnnotations map[string]string
+}
+
+func (h *hasAnnotations) Matches(obj client.Object) bool {
+	objAnnos := obj.GetAnnotations()
+	for k, v := range h.requiredAnnotations {
+		value, present := objAnnos[k]
+		if !present || value != v {
+			return false
+		}
+	}
+	return true
+}
+
+// HasAnnotations returns a predicate checking that an Object has provided annotations and their values.
+func HasAnnotations(requiredAnnotations map[string]string) Predicate[client.Object] {
+	return &hasAnnotations{requiredAnnotations: requiredAnnotations}
+}
