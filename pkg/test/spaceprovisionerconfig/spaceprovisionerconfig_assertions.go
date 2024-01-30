9c1a58d8963c9a85c2f03f0456c4b9b6a0415a4b
Lukas Krejci <lkrejci@redhat.com>
2024-01-30 16:12:24 +0100

    Simplify the predicates using the pre-existing functions in the condition package.

--- pkg/test/spaceprovisionerconfig/spaceprovisionerconfig_assertions.go
+++ pkg/test/spaceprovisionerconfig/spaceprovisionerconfig_assertions.go
@@ -4,7 +4,6 @@ import (
 	toolchainv1alpha1 "github.com/codeready-toolchain/api/api/v1alpha1"
 	"github.com/codeready-toolchain/toolchain-common/pkg/condition"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test/assertions"
-	corev1 "k8s.io/api/core/v1"
 )
 
 type predicate func(*toolchainv1alpha1.SpaceProvisionerConfig) bool
@@ -17,12 +16,7 @@ func (p predicate) Matches(obj *toolchainv1alpha1.SpaceProvisionerConfig) bool {
 
 func Ready() assertions.Predicate[*toolchainv1alpha1.SpaceProvisionerConfig] {
 	return predicate(func(spc *toolchainv1alpha1.SpaceProvisionerConfig) bool {
-		cond, found := condition.FindConditionByType(spc.Status.Conditions, toolchainv1alpha1.ConditionReady)
-		if !found {
-			return false
-		}
-
-		return cond.Status == corev1.ConditionTrue && cond.Reason == toolchainv1alpha1.SpaceProvisionerConfigValidReason && cond.Message == ""
+		return condition.IsTrueWithReason(spc.Status.Conditions, toolchainv1alpha1.ConditionReady, toolchainv1alpha1.SpaceProvisionerConfigValidReason)
 	})
 }
 
@@ -34,11 +28,6 @@ func NotReady() assertions.Predicate[*toolchainv1alpha1.SpaceProvisionerConfig] 
 
 func NotReadyWithReason(reason string) assertions.Predicate[*toolchainv1alpha1.SpaceProvisionerConfig] {
 	return predicate(func(spc *toolchainv1alpha1.SpaceProvisionerConfig) bool {
-		cond, found := condition.FindConditionByType(spc.Status.Conditions, toolchainv1alpha1.ConditionReady)
-		if !found {
-			return false
-		}
-
-		return cond.Status == corev1.ConditionFalse && cond.Reason == reason
+		return condition.IsFalseWithReason(spc.Status.Conditions, toolchainv1alpha1.ConditionReady, reason)
 	})
 }
