879f02df98cf7ae4da15c256c8551e1de847c026
Francisc Munteanu <fmuntean@redhat.com>
2024-03-04 17:18:51 +0100

    check exact match of the objects

--- pkg/template/template_test.go
+++ pkg/template/template_test.go
@@ -7,6 +7,10 @@ import (
 	"github.com/codeready-toolchain/toolchain-common/pkg/template"
 	"github.com/codeready-toolchain/toolchain-common/pkg/test"
 	"github.com/stretchr/testify/require"
+	v1 "k8s.io/api/core/v1"
+	rbac "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 //go:embed testdata/*
@@ -15,30 +19,25 @@ var EFS embed.FS
 //go:embed testdata/host/*
 var hostFS embed.FS
 
+//go:embed testdata/member/*
+var memberFS embed.FS
+
 func TestLoadObjectsFromEmbedFS(t *testing.T) {
 	t.Run("loads objects recursively from all subdirectories", func(t *testing.T) {
 		// when
-		objects, err := template.LoadObjectsFromEmbedFS(&EFS, &template.Variables{Namespace: test.HostOperatorNs})
+		allObjects, err := template.LoadObjectsFromEmbedFS(&EFS, &template.Variables{Namespace: test.HostOperatorNs})
+		hostFolderObjects, err := template.LoadObjectsFromEmbedFS(&hostFS, &template.Variables{Namespace: test.HostOperatorNs})
+		memberFolderObjects, err := template.LoadObjectsFromEmbedFS(&memberFS, nil)
 		// then
 		require.NoError(t, err)
-		require.NotNil(t, objects)
-		// we expect to have 4 objects loaded from the test templates folders
-		require.Equal(t, 4, len(objects), "invalid number of expected objects")
-	})
-
-	t.Run("variable substitution works", func(t *testing.T) {
-		// when
-		// we pass only the hostFS directory
-		objects, err := template.LoadObjectsFromEmbedFS(&hostFS, &template.Variables{Namespace: test.HostOperatorNs})
-		// then
-		// object's in this folder should have variables to be replaced
-		require.NoError(t, err)
-		require.NotNil(t, objects)
-		for _, obj := range objects {
-			// for now we only support the Namespace variable.
-			// let's make sure it's correctly set on all the objects
-			require.Equal(t, test.HostOperatorNs, obj.GetNamespace())
-		}
+		require.NotNil(t, allObjects)
+		require.NotNil(t, hostFolderObjects)
+		require.NotNil(t, memberFolderObjects)
+		require.Equal(t, 4, len(allObjects), "invalid number of expected total objects")
+		require.Equal(t, 3, len(hostFolderObjects), "invalid number of expected objects from host folder")
+		require.Equal(t, 1, len(memberFolderObjects), "invalid number of expected objects from member folder")
+		// check match for the expected objects
+		checkExpectedObjects(t, allObjects)
 	})
 
 	t.Run("error - when variables are not provided", func(t *testing.T) {
@@ -50,4 +49,47 @@ func TestLoadObjectsFromEmbedFS(t *testing.T) {
 		require.Error(t, err)
 		require.Nil(t, objects)
 	})
+}
+
+func checkExpectedObjects(t *testing.T, objects []*unstructured.Unstructured) {
+	sa := &v1.ServiceAccount{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(objects[0].Object, sa)
+	require.NoError(t, err)
+	require.Equal(t, "toolchaincluster-host", sa.GetName())
+	require.Equal(t, "toolchain-host-operator", sa.GetNamespace())
+	role := &rbac.Role{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(objects[1].Object, role)
+	require.Equal(t, "toolchaincluster-host", role.GetName())
+	require.Equal(t, "toolchain-host-operator", role.GetNamespace())
+	require.Equal(t, []rbac.PolicyRule{
+		{
+			APIGroups: []string{"toolchain.dev.openshift.com"},
+			Resources: []string{"*"},
+			Verbs:     []string{"*"},
+		},
+	}, role.Rules)
+	roleBinding := &rbac.RoleBinding{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(objects[2].Object, roleBinding)
+	require.Equal(t, "toolchaincluster-host", roleBinding.GetName())
+	require.Equal(t, "toolchain-host-operator", roleBinding.GetNamespace())
+	require.Equal(t, rbac.RoleRef{
+		APIGroup: "rbac.authorization.k8s.io",
+		Kind:     "Role",
+		Name:     "toolchaincluster-host",
+	}, roleBinding.RoleRef)
+	require.Equal(t, 1, len(roleBinding.Subjects))
+	require.Equal(t, rbac.Subject{
+		Kind: "ServiceAccount",
+		Name: "toolchaincluster-host",
+	}, roleBinding.Subjects[0])
+	clusterRole := &rbac.ClusterRole{}
+	err = runtime.DefaultUnstructuredConverter.FromUnstructured(objects[3].Object, clusterRole)
+	require.Equal(t, "member-toolchaincluster-cr", clusterRole.GetName())
+	require.Equal(t, []rbac.PolicyRule{
+		{
+			APIGroups: []string{"authentication.k8s.io"},
+			Resources: []string{"tokenreviews"},
+			Verbs:     []string{"create"},
+		},
+	}, clusterRole.Rules)
 }
