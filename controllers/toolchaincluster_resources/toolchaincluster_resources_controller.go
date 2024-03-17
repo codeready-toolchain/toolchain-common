b5015a53eb80030dc9c79e03aad19c82073fe6d5
Francisc Munteanu <fmuntean@redhat.com>
2024-03-17 22:09:07 +0100

    remove get of toolchaincluster resource

--- controllers/toolchaincluster_resources/toolchaincluster_resources_controller.go
+++ controllers/toolchaincluster_resources/toolchaincluster_resources_controller.go
@@ -11,7 +11,6 @@ import (
 	commonpredicates "github.com/codeready-toolchain/toolchain-common/pkg/predicate"
 	"github.com/codeready-toolchain/toolchain-common/pkg/template"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
@@ -55,17 +54,6 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 		return reconcile.Result{}, fmt.Errorf("no templates FS configured")
 	}
 
-	// Fetch the ToolchainCluster instance
-	toolchainCluster := &toolchainv1alpha1.ToolchainCluster{}
-	err := r.client.Get(ctx, request.NamespacedName, toolchainCluster)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			return reconcile.Result{}, nil
-		}
-		// Error reading the object - requeue the request.
-		return reconcile.Result{}, err
-	}
-
 	// read the template fs
 	allObjects, err := template.LoadObjectsFromEmbedFS(r.templates, &template.Variables{Namespace: request.Namespace})
 	if err != nil {
@@ -74,7 +62,6 @@ func (r *Reconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.
 	// apply all the objects and add toolchaincluster as owner reference
 	newLabels := map[string]string{
 		toolchainv1alpha1.ProviderLabelKey: toolchainv1alpha1.ProviderLabelValue,
-		toolchainv1alpha1.OwnerLabelKey:    toolchainCluster.GetName(),
 	}
 	_, err = applycl.ApplyUnstructuredObjects(ctx, r.client, allObjects, newLabels)
 	return reconcile.Result{}, err
