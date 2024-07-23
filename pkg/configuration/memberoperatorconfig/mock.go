d6c3d08f7f87818de77314d0320bb98d47a14679
Rafaela Soares <rsoaresd@redhat.com>
2024-07-23 14:06:31 +0100

    gofmt

--- pkg/configuration/memberoperatorconfig/mock.go
+++ pkg/configuration/memberoperatorconfig/mock.go
@@ -1,16 +1,16 @@
 package memberoperatorconfig
 
 import (
-    "github.com/stretchr/testify/mock"
-    "k8s.io/apimachinery/pkg/runtime"
-    "k8s.io/client-go/rest"
+	"github.com/stretchr/testify/mock"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/rest"
 )
 
 type MockConfigGetter struct {
-    mock.Mock
+	mock.Mock
 }
 
 func (m *MockConfigGetter) GetCRTConfiguration(config *rest.Config, scheme *runtime.Scheme) (Configuration, error) {
-    args := m.Called(config, scheme)
-    return args.Get(0).(Configuration), args.Error(1)
+	args := m.Called(config, scheme)
+	return args.Get(0).(Configuration), args.Error(1)
 }
