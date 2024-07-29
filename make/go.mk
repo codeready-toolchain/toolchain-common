4ec5a8d253f624b32b9aa3710eeeb65b4d977ec9
Feny Mehta <fbm3307@gmail.com>
2024-07-29 16:32:27 +0530

    review
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/go.mk
+++ make/go.mk
@@ -30,7 +30,7 @@ vet:
 	go vet ./...
 
 TMP_DIR = /tmp/
-BASE_REPO_PATH = $(shell mktemp -d ${TMP_DIR}crt-verify.XXX)
+BASE_REPO_PATH = $(shell mktemp -d ${TMP_DIR}replace-verify.XXX)
 GH_BASE_URL_KS = https://github.com/kubesaw/
 GH_BASE_URL_CRT = https://github.com/codeready-toolchain/
 GH_KSCTL = $(GH_BASE_URL_KS)ksctl
@@ -38,20 +38,15 @@ GH_HOST = $(GH_BASE_URL_CRT)host-operator
 GH_MEMBER = $(GH_BASE_URL_CRT)member-operator
 GH_REGSVC = $(GH_BASE_URL_CRT)registration-service
 GH_E2E = $(GH_BASE_URL_CRT)toolchain-e2e
-
+GH_TC = $(GH_BASE_URL_CRT)toolchain-common
 
 .PHONY: verify-replace-run
 verify-replace-run:
 	$(eval C_PATH = $(PWD))\
-	$(foreach repo, ${GH_HOST} ${GH_MEMBER} ${GH_REGSVC} ${GH_E2E} ${GH_KSCTL},\
+	$(foreach repo,${GH_HOST},\
 	$(eval REPO_PATH = ${BASE_REPO_PATH}/$(shell basename $(repo))) \
 	git clone --depth=1 $(repo) ${REPO_PATH}; \
 	cd ${REPO_PATH}; \
-	if [[ "${GH_HOST}" == "$(repo)" ]]; then \
-		$(MAKE) generate ; \
-	elif [[ "${GH_MEMBER}" == "$(repo)" ]]; then\
-		$(MAKE) generate-assets ; \
-	fi; \
 	go mod edit -replace github.com/codeready-toolchain/toolchain-common=${C_PATH}; \
-	$(MAKE) verify-dependencies; \
+	$(MAKE) verify-dependencies 1>/dev/null; \
 	)
