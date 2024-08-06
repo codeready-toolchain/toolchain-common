62434ae2fc7713372309c7613cbc3f72d8a37a79
Feny Mehta <fbm3307@gmail.com>
2024-08-06 13:20:40 +0530

    changing it to shell script
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- make/go.mk
+++ make/go.mk
@@ -29,24 +29,6 @@ tidy:
 vet:
 	go vet ./...
 
-TMP_DIR = /tmp/
-BASE_REPO_PATH = $(shell mktemp -d ${TMP_DIR}replace-verify.XXX)
-GH_BASE_URL_KS = https://github.com/kubesaw/
-GH_BASE_URL_CRT = https://github.com/codeready-toolchain/
-GH_KSCTL = $(GH_BASE_URL_KS)ksctl
-GH_HOST = $(GH_BASE_URL_CRT)host-operator
-GH_MEMBER = $(GH_BASE_URL_CRT)member-operator
-GH_REGSVC = $(GH_BASE_URL_CRT)registration-service
-GH_E2E = $(GH_BASE_URL_CRT)toolchain-e2e
-GH_TC = $(GH_BASE_URL_CRT)toolchain-common
-
 .PHONY: verify-replace-run
 verify-replace-run:
-	$(eval C_PATH = $(PWD))\
-	$(foreach repo,${GH_HOST} ${GH_MEMBER} ${GH_REGSVC} ${GH_E2E} ${GH_KSCTL},\
-	$(eval REPO_PATH = ${BASE_REPO_PATH}/$(shell basename $(repo))) \
-	git clone --depth=1 $(repo) ${REPO_PATH}; \
-	cd ${REPO_PATH}; \
-	go mod edit -replace github.com/codeready-toolchain/toolchain-common=${C_PATH}; \
-	$(MAKE) verify-dependencies; \
-	)
+	./verify-replace.sh; 
---
+++ verify-replace.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TMP_DIR=/tmp/
+BASE_REPO_PATH=$(mktemp -d ${TMP_DIR}replace-verify.XXX)
+GH_BASE_URL_KS=https://github.com/kubesaw/
+GH_BASE_URL_CRT=https://github.com/codeready-toolchain/
+GH_KSCTL=${GH_BASE_URL_KS}ksctl
+GH_HOST=${GH_BASE_URL_CRT}host-operator
+GH_MEMBER=${GH_BASE_URL_CRT}member-operator
+GH_REGSVC=${GH_BASE_URL_CRT}registration-service
+GH_E2E=${GH_BASE_URL_CRT}toolchain-e2e
+GH_TC=${GH_BASE_URL_CRT}toolchain-common
+
+C_PATH=${PWD}
+for repo in ${GH_HOST} ${GH_MEMBER} ${GH_REGSVC} ${GH_E2E} ${GH_KSCTL}
+do
+    REPO_PATH=$BASE_REPO_PATH/$(basename $repo)
+    git clone --depth=1 $repo $REPO_PATH
+    cd $REPO_PATH
+    go mod edit -replace github.com/codeready-toolchain/toolchain-common=$C_PATH
+    make verify-dependencies
+done
