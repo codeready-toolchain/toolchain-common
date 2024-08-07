aa868451b517c8cc83980ce4ce00ad21ad697622
Feny Mehta <fbm3307@gmail.com>
2024-08-07 16:31:39 +0530

    converting to shell script,adding error repo names
    Signed-off-by: Feny Mehta <fbm3307@gmail.com>

--- verify-replace.sh
+++ verify-replace.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 TMP_DIR=/tmp/
 BASE_REPO_PATH=$(mktemp -d ${TMP_DIR}replace-verify.XXX)
 GH_BASE_URL_KS=https://github.com/kubesaw/
@@ -10,13 +9,19 @@ GH_MEMBER=${GH_BASE_URL_CRT}member-operator
 GH_REGSVC=${GH_BASE_URL_CRT}registration-service
 GH_E2E=${GH_BASE_URL_CRT}toolchain-e2e
 GH_TC=${GH_BASE_URL_CRT}toolchain-common
-
 C_PATH=${PWD}
-for repo in ${GH_HOST} ${GH_MEMBER} ${GH_REGSVC} ${GH_E2E} ${GH_KSCTL}
+ERRORLIST=()
+
+for repo in ${GH_HOST} ${GH_REGSVC} ${GH_KSCTL} ${GH_MEMBER} ${GH_E2E}
 do
     REPO_PATH=$BASE_REPO_PATH/$(basename $repo)
     git clone --depth=1 $repo $REPO_PATH
     cd $REPO_PATH
     go mod edit -replace github.com/codeready-toolchain/toolchain-common=$C_PATH
-    make verify-dependencies
+    make verify-dependencies || ERRORLIST+=($(basename $repo))
+done
+echo Errors in repos:
+for e in ${ERRORLIST[*]}
+do
+    echo $e
 done
